0dc57bdaa32c7c7b2fab4b4888fc27f08c98faab
italobarzola <italo.barzola@links.com.ec>
2022-07-10 22:37:05 -0500

    campaign

---
+++ src/apps/retention/campaign/domain/Campaign.ts
@@ -0,0 +1,10 @@
+
+export class Campaign {
+    id?: string;
+    title: string;
+    reason: string;
+    image: string;
+    is_public: boolean;
+    created_at?: number;
+    modified_at?: number;
+}
