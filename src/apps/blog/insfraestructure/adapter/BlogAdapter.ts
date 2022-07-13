e7d4eeb30467e85f7043d15654bc38e602dcd8e1
italobarzola <italo.barzola@links.com.ec>
2022-07-13 13:32:01 -0500

    refactor

--- src/apps/blog/insfraestructure/adapter/BlogAdapter.ts
+++ src/apps/blog/insfraestructure/adapter/BlogAdapter.ts
@@ -1,33 +1,45 @@
 import { injectable, inject } from 'inversify'
-import { Blogs } from '../../application/Blogs';
 import { BlogUserInterface } from '../ui/BlogUserInterface'
 import TYPES from '../../../../../container.types'
-import { Comment, Post } from '../../domain/Blog';
+import { Comment, Post, PostPaginate } from '../../domain/Blog';
+import { PostCreate } from '../../application/create/PostCreate';
+import { PostDelete } from '../../application/delete/PostDelete';
+import { PostRetrieveDetail } from '../../application/retrieve-detail/PostRetrieveDetail';
+import { PostAddComment } from '../../application/add-comment/PostAddComment';
+import { PostPublish } from '../../application/publish/PostPublish';
+import { PostUpdate } from '../../application/update/PostUpdate';
+import { PostsPaginate } from '../../application/paginate/PostPaginate';
 
 @injectable()
 export class BlogAdapter {
   constructor(
     @inject(TYPES.BlogUserInterface)
     private blogUserInterface: BlogUserInterface,
-    @inject(TYPES.Blog) private blogs: Blogs
+    @inject(TYPES.BlogPostCreate) private postCreate: PostCreate,
+    @inject(TYPES.BlogPostDelete) private postDelete: PostDelete,
+    @inject(TYPES.BlogPostPaginate) private postPaginate: PostsPaginate,
+    @inject(TYPES.BlogPostRetrieveDetail) private postRetrieveDetail: PostRetrieveDetail,
+    @inject(TYPES.BlogPostAddComment) private postAddComment: PostAddComment,
+    @inject(TYPES.BlogPostPublish) private postPublish: PostPublish,
+    @inject(TYPES.BlogPostUpdate) private postUpdate: PostUpdate
   ) { }
 
   init() {
     this.blogUserInterface.installPostPublish((postUid: string) =>
-      this.blogs.postPublish(postUid)
+      this.postPublish.publish(postUid)
     )
     this.blogUserInterface.installPostCreate((post: Post) =>
-      this.blogs.postCreate(post)
+      this.postCreate.create(post)
     )
     this.blogUserInterface.installPostPaginate((limit: number, next: number) =>
-      this.blogs.postPaginate(limit, next)
+      this.postPaginate.paginate(limit, next)
     )
     this.blogUserInterface.installPostDelete((postUid: string) =>
-      this.blogs.postDelete(postUid)
+      this.postDelete.delete(postUid)
     )
     this.blogUserInterface.installPostRetrieveDetail((postUid: string) =>
-      this.blogs.postRetrieveDetail(postUid))
+      this.postRetrieveDetail.retrieveDetail(postUid))
     this.blogUserInterface.installPostAddComment((postUid: string, comment: Comment) =>
-      this.blogs.postAddComment(postUid, comment))
+      this.postAddComment.addComment(postUid, comment))
   }
 }
