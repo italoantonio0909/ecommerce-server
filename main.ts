3a3bf8c3734298ba6a5a4b0113253e6ccfbc5d24
italobarzola <italo.barzola@links.com.ec>
2022-07-21 12:50:51 -0500

    changues

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata'
+import { BackendApp } from './src/apps/backend/start';
 
+new BackendApp().start();
 // import { container } from './src/apps/sales/subscribers/container';
 
 // container
